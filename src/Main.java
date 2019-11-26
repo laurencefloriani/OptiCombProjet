086611087af305d1b495cccca30adb680e0b3ca5
JonNl11 <37840796+JonNl11@users.noreply.github.com>
2019-11-26 16:31:27 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,20 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        Graph graph = FilesProcessing.readFile("assets/instance1");
-        graph.buildMatrix();
+        Graph graph = FilesProcessing.readFile("assets/instance0");
         graph.printMatrix();
+        List <List<Integer>> list = new ArrayList<List<Integer>>();
+        List <Integer> l = new ArrayList<>();
+        l.add(1);
+        l.add(2);
+        l.add(3);
+        list.add(l);
+
+        List <Integer> l2 = new ArrayList<>();
+        l2.add(4);
+        l2.add(5);
+        l2.add(6);
+        list.add(l2);
+        System.out.println(Simulation.computeM(list, graph.getVertices()));
     }
 }
