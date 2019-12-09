2e548b88ec27ad83f5010b7fe548a167e5a2fe58
JonNl11 <37840796+JonNl11@users.noreply.github.com>
2019-12-09 12:18:21 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,22 @@ public class Main {
     public static void main(String[] args) {
         Graph graph = FilesProcessing.readFile("assets/instance1");
         graph.printMatrix();
+        graph.getBestNodes();
+        ArrayList<Integer[]> temporaire =  new ArrayList<>();
+        ArrayList<Integer> talion= new ArrayList<>();
+        for (int i = 0; i < graph.bestNodes.size() ; i++){
+            temporaire.add(graph.bestNodes.get(i));
 
-        MockAnnealing mock = new MockAnnealing(50);
+        }
+
+
+        // ArrayList<Integer[]> tempo =  graph.makeall(talion)/*graph.completeCommunity(temporaire)*/;
+      /*  for(Integer [] t : tempo){
+            System.out.println("<"+t[0]+","+ t[1]+">");
+        }*/
+        graph.tries(temporaire);
+
+        //MockAnnealing mock = new MockAnnealing(50);
         //System.out.println(sol.toString());
         //System.out.println(sol.getmP());
     }
